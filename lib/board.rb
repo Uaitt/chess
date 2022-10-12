30132dafe7f72e3a5628fb83c3088129b1012315
Lorenzo Zabot <lorenzozabot@gmail.com>
2022-10-12 16:58:42 +0200

    Write tests for top row of #set

--- lib/board.rb
+++ lib/board.rb
@@ -2,7 +2,7 @@
 
 # this class represents the board in chess
 class Board
-  def initialize
+  def set
     dispose_pieces('black')
     dispose_pieces('white')
   end
@@ -20,22 +20,15 @@ class Board
     dispose_pawns(color)
   end
 
+=begin
   def dispose_main_pieces(color)
-    row = color == 'black' ? 0 : 7
-    @data[row][0] = Root.new(color)
-    @data[row][1] = Knight.new(color)
-    @data[row][2] = Bishop.new(color)
-    @data[row][3] = Queen.new(color)
-    @data[row][4] = King.new(color)
-    @data[row][5] = Bishop.new(color)
-    @data[row][6] = Knight.new(color)
-    @data[row][7] = Root.new(color)
   end
 
   def dispose_pawns(color)
     row = color == 'black' ? 1 : 6
     @data[row].map! { Pawn.new(color) }
   end
+=end
 
   def current_position(piece)
     @data.each_with_index do |row, row_index|
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -6,6 +6,45 @@ require_relative '../lib/pieces/knight'
 describe Board do
   subject(:board) { described_class.new }
 
+  describe '#set' do
+    let(:data) { board.instance_variable_get(:@data) }
+    before do
+      board.set
+    end
+    
+    it 'places a rook on the first square of top row' do
+      expect(data[0][0].class).to eq(Rook)
+    end
+
+    it 'places a knight on the second square of tow row' do
+      expect(data[0][1].class).to eq(Knight)
+    end
+
+    it 'places a bishop on the third square of top row' do
+      expect(data[0][2].class).to eq(Bishop)
+    end
+
+    it 'places a queen on the fourth square of top row' do
+      expect(data[0][3].class).to eq(Queen)
+    end
+
+    it 'places a king on the fifth square of top row' do
+      expect(data[0][4].class).to eq(King)
+    end
+
+    it 'places a bishop on the sixth square of top row' do
+      expect(data[0][5].class).to eq(Bishop)
+    end
+
+    it 'places a knight on the seventh square of top row' do
+      expect(data[0][6].class).to eq(Knight)
+    end
+
+    it 'places a rook on the eighth square of top row' do
+      expect(data[0][7].class).to eq(Rook)
+    end
+  end
+
   describe '#move' do
     let(:knight) { Knight.new }
     before do
