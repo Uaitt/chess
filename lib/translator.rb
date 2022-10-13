d1968169416ac8d53b9810490016602badb92cfe
Lorenzo Zabot <lorenzozabot@gmail.com>
2022-10-13 17:26:51 +0200

    Add #translate and its implementation

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,1 +1,18 @@
 # frozen_string_literal: true
+
+# this class represents a translator for chess coordinates and board indexes
+class Translator
+  def translate(coordinate)
+    [convert_number(coordinate[1]), convert_letter(coordinate[0])]
+  end
+
+  private
+
+  def convert_number(number)
+    8 - number.to_i
+  end
+
+  def convert_letter(letter)
+    letter.ord - 97
+  end
+end
--- spec/translator_spec.rb
+++ spec/translator_spec.rb
@@ -7,9 +7,33 @@ describe Translator do
   describe '#translate' do
     context 'when given \'a8\'' do
       it 'returns [0, 0]' do
-        input = 'a8'
-        result = translator.translate(input)
+        coordinate = 'a8'
+        result = translator.translate(coordinate)
         expect(result).to eq([0, 0])
+      end
+    end
+
+    context 'when given \'b8\'' do
+      it 'returns [0, 1]' do
+        coordinate = 'b8'
+        result = translator.translate(coordinate)
+        expect(result).to eq([0, 1])
+      end
+    end
+
+    context 'when given \'a1\'' do
+      it 'returns [7, 0]' do
+        coordinate = 'a1'
+        result = translator.translate(coordinate)
+        expect(result).to eq([7, 0])
+      end
+    end
+
+    context 'when given \'h6\'' do
+      it 'returns [2, 7]' do
+        coordinate = 'h6'
+        result = translator.translate(coordinate)
+        expect(result).to eq([2, 7])
       end
     end
   end
