2dc860f5a7141b9be272aceca8a368969d4faa74
Lorenzo Zabot <lorenzozabot@gmail.com>
2022-10-13 20:40:10 +0200

    Refactor class top level documentation

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-# this class represents a translator for chess coordinates and array indexes
+# turns chess coordinates into array indexes
 class Translator
   def translate(coordinate)
     [convert_number(coordinate[1]), convert_letter(coordinate[0])]
