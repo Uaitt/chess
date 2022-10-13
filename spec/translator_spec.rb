ddc2b2fad8ce5ee63ab888f39811233588f9fc35
Lorenzo Zabot <lorenzozabot@gmail.com>
2022-10-13 17:10:24 +0200

    Create first test

--- spec/translator_spec.rb
+++ spec/translator_spec.rb
@@ -1,4 +1,16 @@
 # frozen_string_literal: true
 
+require_relative '../lib/translator'
+
 describe Translator do
+  subject(:translator) { described_class.new }
+  describe '#translate' do
+    context 'when given \'a8\'' do
+      it 'returns [0, 0]' do
+        input = 'a8'
+        result = translator.translate(input)
+        expect(result).to eq([0, 0])
+      end
+    end
+  end
 end
