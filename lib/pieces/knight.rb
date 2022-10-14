795c1b9d3fe247769b7173618da4840567fdf1e8
Lorenzo Zabot <lorenzozabot@gmail.com>
2022-10-14 16:54:06 +0200

    Add #reachable?

--- lib/pieces/knight.rb
+++ lib/pieces/knight.rb
@@ -6,5 +6,21 @@ class Knight
 
   def initialize(color)
     @color = color
+    @possible_moves = [[1, 2], [1, -2], [-1, 2], [-1, -2],
+                       [2, 1], [2, -1], [-2, 1], [-2, -1]]
+  end
+
+  def reachable?(initial_position, end_position)
+    @possible_moves.each do |current_move|
+      return true if current_move_matches?(initial_position, current_move, end_position)
+    end
+    false
+  end
+
+  private
+
+  def current_move_matches?(initial_position, current_move, end_position)
+    initial_position[0] + current_move[0] == end_position[0] &&
+      initial_position[1] + current_move[1] == end_position[1]
   end
 end
