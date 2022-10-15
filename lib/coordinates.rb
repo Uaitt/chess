e8519b94b4640ba6a2daefb86d632c150c5b4396
Lorenzo Zabot <lorenzozabot@gmail.com>
2022-10-15 14:53:31 +0200

    Add #convert

--- lib/coordinates.rb
+++ lib/coordinates.rb
@@ -2,8 +2,12 @@
 
 # this class represents a pair of coordinates in the chess board
 class Coordinates
-  def initialize (input)
+  def initialize(input)
     @letter = input[0]
     @number = input[1]
+  end
+
+  def convert
+    [8 - @number.to_i, @letter.ord - 97]
   end
 end
