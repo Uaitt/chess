dafbfa722cfffe2cf0cd55ef19ab1daac850196c
Lorenzo Zabot <lorenzozabot@gmail.com>
2022-10-17 15:09:32 +0200

    Create boilerplate for #able_to_reach?

--- lib/pieces/rook.rb
+++ lib/pieces/rook.rb
@@ -6,5 +6,19 @@ class Rook
 
   def initialize(color)
     @color = color
+    @possible_moves = create_possible_moves
+  end
+
+  def able_to_reach?(current_position, end_position)
+    @possible_moves.each do |move|
+      return true if current_move_matches?(current_position, move, end_position)
+    end
+
+    false
+  end
+
+  private
+
+  def create_possible_moves
   end
 end
