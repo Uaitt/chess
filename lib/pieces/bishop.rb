d9ef55f580b0c5a36541ba208abcceb85c6d28d8
Lorenzo Zabot <lorenzozabot@gmail.com>
2022-10-18 21:16:05 +0200

    Add #blocked?

--- lib/pieces/bishop.rb
+++ lib/pieces/bishop.rb
@@ -16,6 +16,13 @@ class Bishop
     false
   end
 
+  def blocked?(board, current_position, end_position)
+    base_move = base_move(current_position, end_position)
+
+    blocked_on_path?(board, base_move, current_position, end_position) ||
+      blocked_on_final_square?(board, end_position)
+  end
+
   private
 
   def create_possible_moves
@@ -27,6 +34,12 @@ class Bishop
       current_position[1] + move[1] == end_position[1]
   end
 
+  def base_move(current_position, end_position)
+    move = end_position.zip(current_position).map { |finish, current| finish - current }
+    factor = move.max.abs
+    move.map { |item| item / factor }
+  end
+
   def left_to_right_moves
     [[1, 1], [2, 2], [3, 3], [4, 4], [5, 5], [6, 6], [7, 7],
      [-1, -1], [-2, -2], [-3, -3], [-4, -4], [-5, -5], [-6, -6], [-7, -7]]
@@ -35,5 +48,20 @@ class Bishop
   def right_to_left_moves
     [[1, -1], [2, -2], [3, -3], [4, -4], [5, -5], [6, -6], [7, -7],
      [-1, 1], [-2, 2], [-3, 3], [-4, 4], [-5, 5], [-6, 6], [-7, 7]]
+  end
+
+  def blocked_on_path?(board, base_move, current_position, end_position)
+    current_position = current_position.zip(base_move).map(&:sum)
+    until current_position == end_position
+      return true unless board.data[current_position[0]][current_position[1]].nil?
+
+      current_position = current_position.zip(base_move).map(&:sum)
+    end
+    false
+  end
+
+  def blocked_on_final_square?(board, end_position)
+    !board.data[end_position[0]][end_position[1]].nil? &&
+      board.data[end_position[0]][end_position[1]].color == @color
   end
 end
