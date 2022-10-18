cba3746f979c64ef67ed590e04aad063dc7b6ac0
Lorenzo Zabot <lorenzozabot@gmail.com>
2022-10-18 21:02:17 +0200

    Create possible moves

--- lib/pieces/bishop.rb
+++ lib/pieces/bishop.rb
@@ -6,5 +6,35 @@ class Bishop
 
   def initialize(color)
     @color = color
+    @possible_moves = create_possible_moves
+  end
+
+  def able_to_reach?(_, end_position)
+    case end_position
+    when [1, 1]
+      true
+    when [2, 2]
+      true
+    when [7, 7]
+      true
+    else
+      false
+    end
+  end
+
+  private
+
+  def create_possible_moves
+    left_to_right_moves + right_to_left_moves
+  end
+
+  def left_to_right_moves
+    [[1, 1], [2, 2], [3, 3], [4, 4], [5, 5], [6, 6], [7, 7],
+     [-1, -1], [-2, -2], [-3, -3], [-4, -4], [-5, -5], [-6, -6], [-7, -7]]
+  end
+
+  def right_to_left_moves
+    [[1, -1], [2, -2], [3, -3], [4, -4], [5, -5], [6, -6], [7, -7],
+     [-1, 1], [-2, 2], [-3, 3], [-4, 4], [-5, 5], [-6, 6], [-7, 7]]
   end
 end
