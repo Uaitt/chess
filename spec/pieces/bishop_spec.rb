e6891f04b09427ce4f7051d701e380405b074cdc
Lorenzo Zabot <lorenzozabot@gmail.com>
2022-10-18 21:15:56 +0200

    Add tests for #blocked?

--- spec/pieces/bishop_spec.rb
+++ spec/pieces/bishop_spec.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative '../../lib/pieces/bishop'
+require_relative '../../lib/pieces/knight'
 require_relative '../../lib/board'
 
 describe Bishop do
@@ -55,6 +56,48 @@ describe Bishop do
 
       it 'returns false' do
         expect(bishop.able_to_reach?([7, 0], [5, 3])).to eq(false)
+      end
+    end
+  end
+
+  describe '#blocked?' do
+    let(:board) { Board.new }
+    let(:data) { board.instance_variable_get(:@data) }
+    before do
+      data[0][0] = bishop
+    end
+
+    context 'when it is blocked by a piece of the same color in the path' do
+      it 'returns true' do
+        data[2][2] = Knight.new(:white)
+        expect(bishop.blocked?(board, [0, 0], [5, 5])).to eq(true)
+      end
+    end
+
+    context 'when it is blocked by a piece of the opposite color in the path' do
+      it 'returns true' do
+        data[4][4] = Knight.new(:black)
+        expect(bishop.blocked?(board, [0, 0], [5, 5])).to eq(true)
+      end
+    end
+
+    context 'when it is blocked by a piece of the same color in the last square' do
+      it 'returns true' do
+        data[5][5] = Knight.new(:white)
+        expect(bishop.blocked?(board, [0, 0], [5, 5])).to eq(true)
+      end
+    end
+
+    context 'when it is blocked by a piece of the opposite color in the last square' do
+      it 'returns false' do
+        data[5][5] = Knight.new(:black)
+        expect(bishop.blocked?(board, [0, 0], [5, 5])).to eq(false)
+      end
+    end
+
+    context 'when it is not blocked by any piece' do
+      it 'returns false' do
+        expect(bishop.blocked?(board, [0, 0], [5, 5])).to eq(false)
       end
     end
   end
