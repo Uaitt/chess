c9cb963702da005adc749ecbe3d3d5e9f5b66eee
Lorenzo Zabot <lorenzozabot@gmail.com>
2022-10-18 13:23:19 +0200

    Implement #blocked?

--- lib/board.rb
+++ lib/board.rb
@@ -2,6 +2,8 @@
 
 # this class represents the board in chess
 class Board
+  attr_reader :data
+
   def initialize
     @data = Array.new(8) { Array.new(8, nil) }
   end
--- lib/pieces/rook.rb
+++ lib/pieces/rook.rb
@@ -20,9 +20,9 @@ class Rook
     false
   end
 
-  def blocked?(board, end_position)
-    blocked_on_final_square?(board, end_position) &&
-      blocked_on_path_to_final_square?(board)
+  def blocked?(board, current_position, end_position)
+    blocked_on_its_way_to_final_square?(board, current_position, end_position) ||
+      blocked_on_final_square?(board, end_position)
   end
 
   private
@@ -49,5 +49,18 @@ class Rook
   def current_move_matches?(current_position, move, end_position)
     current_position[0] + move[0] == end_position[0] &&
       current_position[1] + move[1] == end_position[1]
+  end
+
+  def blocked_on_its_way_to_final_square?(board, current_position, end_position)
+    until current_position == end_position
+      return true unless board.data[current_position[0]][current_position[1]].nil?
+
+      current_position = current_position.zip(base_move).map(&:sum)
+    end
+    false
+  end
+
+  def blocked_on_final_square?(board, end_position)
+      board.data[end_position[0]][end_position[1]].color == @color
   end
 end
