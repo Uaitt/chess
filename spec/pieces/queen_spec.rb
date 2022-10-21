f2d965c45847773f243ebf2dbf4b304e4d6c9eb6
Lorenzo Zabot <lorenzozabot@gmail.com>
2022-10-21 15:58:45 +0200

    Add tests for #blocked?

--- spec/pieces/queen_spec.rb
+++ spec/pieces/queen_spec.rb
@@ -87,4 +87,46 @@ describe Queen do
       end
     end
   end
+
+  describe '#blocked?' do
+    let(:board) { Board.new }
+    let(:data) { board.instance_variable_get(:@data) }
+    before do
+      data[0][0] = queen
+    end
+
+    context 'when it is blocked by a piece of the same color in the path' do
+      it 'returns true' do
+        data[2][0] = Knight.new(:white)
+        expect(rook.blocked?(board, [0, 0], [5, 0])).to eq(true)
+      end
+    end
+
+    context 'when it is blocked by a piece of the opposite color in the path' do
+      it 'returns true' do
+        data[4][4] = Knight.new(:black)
+        expect(rook.blocked?(board, [0, 0], [5, 5])).to eq(true)
+      end
+    end
+
+    context 'when it is blocked by a piece of the same color in the last square' do
+      it 'returns true' do
+        data[5][0] = Knight.new(:white)
+        expect(rook.blocked?(board, [0, 0], [5, 0])).to eq(true)
+      end
+    end
+
+    context 'when it is blocked by a piece of the opposite color in the last square' do
+      it 'returns false' do
+        data[5][5] = Knight.new(:black)
+        expect(rook.blocked?(board, [0, 0], [5, 5])).to eq(false)
+      end
+    end
+
+    context 'when it is not blocked by any piece' do
+      it 'returns false' do
+        expect(rook.blocked?(board, [0, 0], [5, 0])).to eq(false)
+      end
+    end
+  end
 end
