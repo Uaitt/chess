72a1f4f95a6f8163c83c3ef08713bad09e37c66e
Lorenzo Zabot <lorenzozabot@gmail.com>
2022-10-21 15:38:32 +0200

    Add tests for #able_ot_reach?

--- spec/pieces/queen_spec.rb
+++ spec/pieces/queen_spec.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 require_relative '../../lib/pieces/queen'
+require_relative '../../lib/pieces/rook'
+require_relative '../../lib/pieces/bishop'
 
 describe Queen do
   subject(:queen) { described_class.new(:white) }
@@ -13,11 +15,63 @@ describe Queen do
       it 'returns true' do
         expect(queen.able_to_reach?([0, 0], [1, 1])).to eq(true)
       end
+
+      it 'returns true' do
+        expect(queen.able_to_reach?([0, 0], [0, 1])).to eq(true)
+      end
+
+      it 'returns true' do
+        expect(queen.able_to_reach?([4, 3], [0, 3])).to eq(true)
+      end
+
+      it 'returns true' do
+        expect(queen.able_to_reach?([4, 3], [7, 3])).to eq(true)
+      end
+
+      it 'returns true' do
+        expect(queen.able_to_reach?([4, 3], [4, 0])).to eq(true)
+      end
+
+      it 'returns true' do
+        expect(queen.able_to_reach?([4, 3], [4, 7])).to eq(true)
+      end
+
+      it 'returns true' do
+        expect(queen.able_to_reach?([4, 3], [1, 0])).to eq(true)
+      end
+
+      it 'returns true' do
+        expect(queen.able_to_reach?([4, 3], [7, 0])).to eq(true)
+      end
+
+      it 'returns true' do
+        expect(queen.able_to_reach?([4, 3], [7, 6])).to eq(true)
+      end
+
+      it 'returns true' do
+        expect(queen.able_to_reach?([4, 3], [0, 7])).to eq(true)
+      end
     end
 
     context 'when the position is not reachable' do
       it 'returns false' do
         expect(queen.able_to_reach?([0, 0], [1, 2])).to eq(false)
+      end
+
+      it 'returns false' do
+        expect(queen.able_to_reach?([4, 3], [0, 0])).to eq(false)
+      end
+
+      it 'returns false' do
+        expect(queen.able_to_reach?([4, 3], [7, 7])).to eq(false)
+      end
+
+      it 'returns false' do
+        expect(queen.able_to_reach?([4, 3], [1, 5])).to eq(false)
+      end
+
+      it 'returns false' do
+        expect(queen.able_to_reach?([4, 3], [3, 1])).to eq(false)
       end
     end
   end
