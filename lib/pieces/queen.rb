6c4399590f9043f422ce3a6c9f8da7508ec87fcf
Lorenzo Zabot <lorenzozabot@gmail.com>
2022-10-21 15:56:22 +0200

    Stat #blocked? implementation

--- lib/pieces/queen.rb
+++ lib/pieces/queen.rb
@@ -16,6 +16,13 @@ class Queen
     false
   end
 
+  def blocked?(board, current_position, end_position)
+    base_move = base_move(current_position, end_position)
+
+    blocked_on_path?(board, base_move, current_position, end_position) ||
+      blocked_on_final_square?(board, end_position)
+  end
+
   private
 
   def create_possible_moves
@@ -25,5 +32,20 @@ class Queen
   def reaches_position?(current_position, move, end_position)
     current_position[0] + move[0] == end_position[0] &&
       current_position[1] + move[1] == end_position[1]
+  end
+
+  def blocked_on_path?(board, base_move, current_position, end_position)
+    current_position = current_position.zip(base_move).map(&:sum)
+    until current_position == end_position
+      return true unless board.data[current_position[0]][current_position[1]].nil?
+
+      current_position = current_position.zip(base_move).map(&:sum)
+    end
+    false
+  end
+
+  def blocked_on_final_square?(board, end_position)
+    !board.data[end_position[0]][end_position[1]].nil? &&
+      board.data[end_position[0]][end_position[1]].color == @color
   end
 end
