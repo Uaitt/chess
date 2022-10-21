41c1a76382ff3f2a03ba9f02d6df2ee0ca737838
Lorenzo Zabot <lorenzozabot@gmail.com>
2022-10-21 15:42:44 +0200

    Add attr_reader for @possible_moves

--- lib/pieces/queen.rb
+++ lib/pieces/queen.rb
@@ -2,18 +2,28 @@
 
 # this class represents a queen in chess
 class Queen
-  attr_accessor :color
+  attr_accessor :color, :possible_moves
 
   def initialize(color)
     @color = color
+    @possible_moves = create_possible_moves
   end
 
-  def able_to_reach?(_, end_position)
-    case end_position
-    when [1, 0]
-      true
-    else
-      false
+  def able_to_reach?(current_position, end_position)
+    possible_moves.each do |move|
+      return true if reaches_position?(current_position, move, end_position)
     end
+    false
+  end
+
+  private
+
+  def create_possible_moves
+    Rook.new(color).possible_moves + Bishop.new(color).possible_moves
+  end
+
+  def reaches_position?(current_position, move, end_position)
+    current_position[0] + move[0] == end_position[0] &&
+      current_position[1] + move[1] == end_position[1]
   end
 end
