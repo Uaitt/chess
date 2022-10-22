9bfd83854ffb983b40c13e2802240186180fb64a
Lorenzo Zabot <lorenzozabot@gmail.com>
2022-10-22 16:01:57 +0200

    Add class boilerplate

---
+++ lib/movements/basic_movements/king_movement.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+# this class represents a rook movement in chess
+class KingMovement < BasicMovement
+end
---
+++ lib/movements/basic_movements/queen_movement.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+# this class represents a rook movement in chess
+class QueenMovement < BasicMovement
+end
