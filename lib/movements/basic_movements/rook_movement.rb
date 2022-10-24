3b2f15f59ee4815b4e960a0e696d655c2bd7860f
Lorenzo Zabot <lorenzozabot@gmail.com>
2022-10-24 15:26:36 +0200

    Add direction_vector

--- lib/movements/basic_movements/rook_movement.rb
+++ lib/movements/basic_movements/rook_movement.rb
@@ -4,4 +4,9 @@ require_relative 'basic_movement'
 
 # this class represents a rook movement in chess
 class RookMovement < BasicMovement
+  def direction_vector
+    direction = @end_position.zip(@initial_position).map { |finish, start| finish - start }
+    factor = direction.reject(&:zero?).first.abs
+    Vector.elements(direction.map { |component| component / factor })
+  end
 end
