ee0e5a56ccc277f1f466527f39d0a868ef03e803
Lorenzo Zabot <lorenzozabot@gmail.com>
2022-10-24 17:11:13 +0200

    Add #direction

--- lib/movements/basic_movements/bishop_movement.rb
+++ lib/movements/basic_movements/bishop_movement.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
-# this class represents a rook movement in chess
-class BihsopMovement < BasicMovement
+# class that represents a bishop movement in chess
+class BiShopMovement < BasicMovement
+  def direction
+    direction = @end_position.zip(@initial_position).map { |finish, start| finish - start }
+    factor = direction.max.abs
+    direction.map { |component| component / factor }
+  end
 end
