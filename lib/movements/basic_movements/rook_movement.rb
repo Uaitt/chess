c037ea89b1ebacefcb71eda4840a450b18066335
Lorenzo Zabot <lorenzozabot@gmail.com>
2022-10-24 16:10:53 +0200

    Add #direction

--- lib/movements/basic_movements/rook_movement.rb
+++ lib/movements/basic_movements/rook_movement.rb
@@ -1,13 +1,12 @@
 # frozen_string_literal: true
 
-require 'matrix'
 require_relative 'basic_movement'
 
 # this class represents a rook movement in chess
 class RookMovement < BasicMovement
-  def direction_vector
+  def direction
     direction = @end_position.zip(@initial_position).map { |finish, start| finish - start }
     factor = direction.reject(&:zero?).first.abs
-    Vector.elements(direction.map { |component| component / factor })
+    direction.map { |component| component / factor }
   end
 end
