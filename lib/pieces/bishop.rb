0d8179b34433fd0ff8c4006b8c84b652ee619953
Lorenzo Zabot <lorenzozabot@gmail.com>
2022-10-25 17:18:52 +0200

    Refactor basic_moves

--- lib/pieces/bishop.rb
+++ lib/pieces/bishop.rb
@@ -8,12 +8,12 @@ class Bishop
     @color = color
   end
 
-  private
-
   def basic_moves
     towards_bottom_right_corner_moves + towards_top_left_corner_moves +
       towards_bottom_left_corner_moves + toward_top_right_corner_moves
   end
+
+  private
 
   def towards_bottom_right_corner_moves
     [[1, 1], [2, 2], [3, 3], [4, 4], [5, 5], [6, 6], [7, 7]]
--- lib/pieces/king.rb
+++ lib/pieces/king.rb
@@ -2,17 +2,14 @@
 
 # this class represents a king in chess
 class King
-  attr_reader :color, :possible_moves
+  attr_reader :color
 
   def initialize(color)
     @color = color
-    @possible_moves = create_possible_moves
   end
 
-  private
-
-  def create_possible_moves
+  def basic_moves
     [[1, 0], [-1, 0], [0, 1], [0, -1],
-      [1, 1], [-1, 1], [1, -1], [-1, -1]]
+     [1, 1], [-1, 1], [1, -1], [-1, -1]]
   end
 end
