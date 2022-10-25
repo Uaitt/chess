34bec6f9f2a9438ff3acb6a15272af1d43746004
Lorenzo Zabot <lorenzozabot@gmail.com>
2022-10-25 15:51:09 +0200

    Add tests for #possible?

--- spec/movements/basic_movements/queen_movement_spec.rb
+++ spec/movements/basic_movements/queen_movement_spec.rb
@@ -12,6 +12,100 @@ describe QueenMovement do
 
   describe '#possible?' do
     context 'when the queen is placed at the top right corner' do
+      before do
+        board.data[0][0] = queen
+      end
+
+      context 'when the movement is one step towards bottom' do
+        let(:end_position) { [1, 0]}
+        it { is_expected.to be_possible }
+      end
+
+      context 'when  the movement is one step towards the right' do
+        let(:end_position) { [0, 1]}
+        it { is_expected.to be_possible }
+      end
+
+      context 'when  the movement is one step towards bottom right corner' do
+        let(:end_position) { [1, 1]}
+        it { is_expected.to be_possible }
+      end
+
+      context 'when  the movement is one step towards bottom and two towards right' do
+        let(:end_position) { [1, 2]}
+        it { is_expected.not_to be_possible }
+      end
+
+      context 'when  the movement is one step towards bottom and three towards right' do
+        let(:end_position) { [1, 3]}
+        it { is_expected.not_to be_possible }
+      end
+
+      context 'when  the movement is three step towards bottom and one towards right' do
+        let(:end_position) { [3, 1]}
+        it { is_expected.not_to be_possible }
+      end
+    end
+
+    context 'when the queen is placed in a random position' do
+      before do
+        board.data[3][4] = queen
+      end
+
+      context 'when the movement is one step towards bottom' do
+        let(:end_position) { [4, 4]}
+        it { is_expected.to be_possible }
+      end
+
+      context 'when the movement is one step towards top' do
+        let(:end_position) { [2, 4]}
+        it { is_expected.to be_possible }
+      end
+
+      context 'when the movement is one step towards right' do
+        let(:end_position) { [3, 5]}
+        it { is_expected.to be_possible }
+      end
+
+      context 'when the movement is one step towards left' do
+        let(:end_position) { [3, 3]}
+        it { is_expected.to be_possible }
+      end
+
+      context 'when the movement is one step towards top left corner' do
+        let(:end_position) { [2, 3]}
+        it { is_expected.to be_possible }
+      end
+
+      context 'when the movement is one step towards top right corner' do
+        let(:end_position) { [2, 5]}
+        it { is_expected.to be_possible }
+      end
+
+      context 'when the movement is one step towards bottom right corner' do
+        let(:end_position) { [4, 5]}
+        it { is_expected.to be_possible }
+      end
+
+      context 'when the movement is one step towards bottom left corner' do
+        let(:end_position) { [4, 3]}
+        it { is_expected.to be_possible }
+      end
+
+      context 'when the movement is one step towards bottom and two towards right' do
+        let(:end_position) { [4, 6]}
+        it { is_expected.not_to be_possible }
+      end
+
+      context 'when the movement is one step towards top and two towards left' do
+        let(:end_position) { [2, 2]}
+        it { is_expected.not_to be_possible }
+      end
+
+      context 'when the movement is two steps towards top and three towards right' do
+        let(:end_position) { [2, 7]}
+        it { is_expected.not_to be_possible }
+      end
     end
   end
 end
