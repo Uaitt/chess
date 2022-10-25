f9c3b84542ee1ba0acad1f5868547f05d7fe4020
Lorenzo Zabot <lorenzozabot@gmail.com>
2022-10-25 17:05:05 +0200

    Add attr_reader for possible_moves

--- lib/pieces/king.rb
+++ lib/pieces/king.rb
@@ -2,17 +2,17 @@
 
 # this class represents a king in chess
 class King
-  attr_reader :color
+  attr_reader :color, :possible_moves
 
   def initialize(color)
     @color = color
     @possible_moves = create_possible_moves
+  end
 
-    private
+  private
 
-    def create_possible_moves
-      [[1, 0], [-1, 0], [0, 1], [0, -1],
-       [1, 1], [-1, 1], [1, -1], [-1, -1]]
-    end
+  def create_possible_moves
+    [[1, 0], [-1, 0], [0, 1], [0, -1],
+      [1, 1], [-1, 1], [1, -1], [-1, -1]]
   end
 end
