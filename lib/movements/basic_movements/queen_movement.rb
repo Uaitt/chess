d849f2f95922eb59add6e2d32510778ce7a0c1c8
Lorenzo Zabot <lorenzozabot@gmail.com>
2022-10-25 16:04:09 +0200

    Implement #direction

--- lib/movements/basic_movements/queen_movement.rb
+++ lib/movements/basic_movements/queen_movement.rb
@@ -1,5 +1,26 @@
 # frozen_string_literal: true
 
+require_relative 'basic_movement'
+require_relative 'rook_movement'
+require_relative 'bishop_movement'
+
 # this class represents a rook movement in chess
 class QueenMovement
+
+  include BasicMovement
+
+  def direction
+    if vertical_or_horizontal?
+      RookMovement
+    else
+      BishopMovement
+    end.new(@board, @piece, @end_position).direction
+  end
+
+  private
+
+  def vertical_or_horizontal?
+    @initial_position[0] == @end_position[0] ||
+      @initial_position[1] == @initial_position[1]
+  end
 end
