2fcb965bfe3a961b8a7d633d768b0af48f86d7db
Lorenzo Zabot <lorenzozabot@gmail.com>
2022-10-25 16:15:38 +0200

    Remove duplicated methods

--- lib/pieces/knight.rb
+++ lib/pieces/knight.rb
@@ -9,26 +9,10 @@ class Knight
     @possible_moves = create_possible_moves
   end
 
-  def able_to_reach?(current_position, end_position)
-    @possible_moves.each do |move|
-      return true if current_move_matches?(current_position, move, end_position)
-    end
-    false
-  end
-
-  def not_blocked?(_, _, _)
-    true
-  end
-
   private
 
   def create_possible_moves
     [[1, 2], [1, -2], [-1, 2], [-1, -2],
      [2, 1], [2, -1], [-2, 1], [-2, -1]]
-  end
-
-  def current_move_matches?(current_position, move, end_position)
-    current_position[0] + move[0] == end_position[0] &&
-      current_position[1] + move[1] == end_position[1]
   end
 end
