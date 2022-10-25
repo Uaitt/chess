4632680faec23fd32495880e266dffca89a4acaf
Lorenzo Zabot <lorenzozabot@gmail.com>
2022-10-25 16:25:54 +0200

    Implement #direction

--- lib/movements/basic_movements/king_movement.rb
+++ lib/movements/basic_movements/king_movement.rb
@@ -2,4 +2,20 @@
 
 # this class represents a king movement in chess
 class KingMovement
+  include BasicMovement
+
+  def direction
+    if vertical_or_horizontal?
+      RookMovement
+    else
+      BishopMovement
+    end.new(@board, @piece, @end_position).direction
+  end
+
+  private
+
+  def vertical_or_horizontal?
+    @initial_position[0] == @end_position[0] ||
+    @initial_position[1] == @initial_position[1]
+  end
 end
