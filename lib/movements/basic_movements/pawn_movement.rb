014eb34e9851f58e0281e8c4dbaffbaba294adbf
Lorenzo Zabot <lorenzozabot@gmail.com>
2022-10-26 15:00:04 +0200

    Allow double move if movement didn't already happen

--- lib/movements/basic_movements/pawn_movement.rb
+++ lib/movements/basic_movements/pawn_movement.rb
@@ -8,17 +8,29 @@ class PawnMovement
 
   def possible?
     @piece.basic_moves.each_with_index do |move, count|
-      return true if reaches_position?(move) && arrival_clear?(count)
+      return true if reaches_position?(move) && environment_allows?(count)
     end
     false
   end
 
   private
 
-  def arrival_clear?(count)
-    return true if count.zero?
+  def environment_allows?(count)
+    arrival_allows?(count) && turn_allows?(count)
+  end
+
+  def arrival_allows?(count)
+    return true if count.zero? || count == 1
 
     !@board.data[@end_position[0]][@end_position[1]].nil? &&
       @board.data[@end_position[0]][@end_position[1]].color != @piece.color
+  end
+
+  def turn_allows?(count)
+    count.zero? ? !already_happened? : true
+  end
+
+  def already_happened?
+    @initial_position[0] != (@piece.color == :black ? 1 : 6)
   end
 end
