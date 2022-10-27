8012b30c3fed53ba102e33a176f9ea6e4c4210d1
Lorenzo Zabot <lorenzozabot@gmail.com>
2022-10-27 20:53:30 +0200

    Refactor

--- lib/pieces/nil_piece.rb
+++ lib/pieces/nil_piece.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-# this class represents a non existing piece
+# this class represents a non existing piece in chess
 class NilPiece
   def self.starts_at?(_, _)
     true
