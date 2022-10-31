c595f42066ade38bc7b0ce2f2ab21df16b1fca44
Lorenzo Zabot <lorenzozabot@gmail.com>
2022-10-31 16:02:58 +0100

    Refactor names

--- lib/movements/special_movements/en_passant/black_en_passant.rb
+++ lib/movements/special_movements/en_passant/black_en_passant.rb
@@ -2,17 +2,17 @@
 
 # ...
 class BlackEnPassant
-  def initialize(board, pawn, end_position)
+  def initialize(board, black_pawn, end_position)
     @board = board
-    @pawn = pawn
-    @initial_position = board.current_position(pawn)
+    @black_pawn = black_pawn
+    @initial_position = board.current_position(black_pawn)
     @left_piece = @board.data[@initial_position[0]][@initial_position[1] - 1]
     @right_piece = @board.data[@initial_position[0]][@initial_position[1] + 1]
     @end_position = end_position
   end
 
   def valid?
-    right_rank? && adjacent_white_pawn? && adjacent_white_pawn_just_double_moved?
+    right_rank? && next_to_white_pawn? && white_pawn_just_double_moved?
   end
 
   private
@@ -21,7 +21,14 @@ class BlackEnPassant
     @initial_position[0] == 4
   end
 
-  def adjacent_white_pawn?
+  def next_to_white_pawn?
     @left_piece.instance_of?(WhitePawn) || @right_piece.instance_of?(WhitePawn)
+  end
+
+  def white_pawn_just_double_moved?
+  end
+
+  def white_pawn
+    @left_piece.instance_of?(WhitePawn) ? @left_piece : @right_piece
   end
 end
