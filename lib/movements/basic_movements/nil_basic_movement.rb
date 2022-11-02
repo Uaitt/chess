a5f9626e64ee1d3b2ff1d02325a216cfab30ef92
Lorenzo Zabot <lorenzozabot@gmail.com>
2022-11-02 21:56:03 +0100

    Encapsulate class method into block

--- lib/movements/basic_movements/nil_basic_movement.rb
+++ lib/movements/basic_movements/nil_basic_movement.rb
@@ -1,10 +1,14 @@
+# frozen_string_literal: true
+
+# this class represents a non existing movement in chess
 class NilBasicMovement
-  def self.handles?(_)
-    true
+  class << self
+    def handles?(_)
+      true
+    end
   end
 
-  def initialize(_,_,_)
-  end
+  def initialize(_, _, _); end
 
   def valid?
     false
