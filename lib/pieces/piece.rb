a72c0ca6f505bc0adf4ee97a8dfda09cc149a43a
Lorenzo Zabot <lorenzozabot@gmail.com>
2022-11-02 21:48:59 +0100

    Encapsulate class methods into block

--- lib/pieces/piece.rb
+++ lib/pieces/piece.rb
@@ -2,12 +2,16 @@
 
 # set of methods common to all pieces in chess
 module Piece
-  def self.for(rank, file)
-    registry.find { |candidate| candidate.starts_at?(rank, file) }.new
-  end
+  class << self
+    def for(rank, file)
+      registry.find { |candidate| candidate.starts_at?(rank, file) }.new
+    end
 
-  def self.registry
-    [BlackBishop, BlackKing, BlackKnight, BlackPawn, BlackQueen, BlackRook,
-     WhiteBishop, WhiteKing, WhiteKnight, WhitePawn, WhiteQueen, WhiteRook, NilPiece]
+    private
+
+    def registry
+      [BlackBishop, BlackKing, BlackKnight, BlackPawn, BlackQueen, BlackRook,
+       WhiteBishop, WhiteKing, WhiteKnight, WhitePawn, WhiteQueen, WhiteRook, NilPiece]
+    end
   end
 end
