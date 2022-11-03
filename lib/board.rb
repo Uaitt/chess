9c43269a258273b9ff44e6d2238d347e34791d2c
Lorenzo Zabot <lorenzozabot@gmail.com>
2022-11-03 15:32:32 +0100

    Remove redundant boilerplate

--- lib/board.rb
+++ lib/board.rb
@@ -25,8 +25,6 @@ class Board
   end
 
   def checked?(king)
-    return false if current_position(king.opposite_color).nil? # change in mated(king.opposite_color)?
-
     @data.any? do |row|
       row.any? do |piece|
         BasicMovement.for(self, piece, current_position(king)).valid?
