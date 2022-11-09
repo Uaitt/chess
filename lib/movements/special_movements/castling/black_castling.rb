bcf5e4677dcf025254d7374642c05ee725443803
Lorenzo Zabot <lorenzozabot@gmail.com>
2022-11-09 14:32:32 +0100

    Refactor

--- lib/movements/special_movements/castling/black_castling.rb
+++ lib/movements/special_movements/castling/black_castling.rb
@@ -4,14 +4,32 @@
 class BlackCastling
   def initialize(board, black_king, end_position)
     @board = board
-    @black_king = black_king
+    @black_king = black_king # the first piece to be moved has to be the king for castling
     @end_position = end_position
-    @black_rook = @board.data[0][file]
+    @piece = @board.data[0][file]
+  end
+
+  def valid?
+    valid_position? && !king_already_moved? && !rook_already_moved? &&
+      !@board.checked?(:black) && @board.allows_castling?(:black) &&
+        !checks_own_king? && empty_squares?
   end
 
   private
 
   def file
     @end_position == [0, 2] ? 0 : 7
+  end
+
+  def valid_position?
+    @end_position == [0, 2] || @end_position == [0, 6]
+  end
+
+  def king_already_moved?
+    @board.current_position(@black_king) != [0, 4]
+  end
+
+  def rook_already_moved?
+    @piece.class != BlackRook
   end
 end
--- spec/movements/special_movements/castling/black_castling_spec.rb
+++ spec/movements/special_movements/castling/black_castling_spec.rb
@@ -5,19 +5,67 @@ require 'require_all'
 require_all 'lib'
 
 describe BlackCastling do
-  subject(:black_castling) { described_class.new(board, black_king, end_position) }
+  subject(:subject) { described_class.new(board, black_king, end_position) }
   let(:board) { Board.new }
   let(:black_king) { BlackKing.new }
   describe '#valid?' do
+    before do
+      board.instance_variable_set(:@data, Array.new(8) { Array.new(8, NilPiece.new) })
+    end
     context 'when moving to the left' do
       let(:end_position) { [0, 2] }
+      context 'when the king was never moved' do
+        before do
+          board.data[0][4] = black_king
+        end
+
+        context 'when the rook has already moved' do
+          before do
+            board.data[1][0] = BlackRook.new
+          end
+
+          it { is_expected.not_to be_valid }
+        end
+
+        context 'when the rook was never moved' do
+          before do
+            board.data[0][0] = BlackRook.new
+          end
+
+          context 'when the king is not in check' do
+          end
+
+          context 'when the king is currently in check' do
+            before do
+              board.data[0][5] = WhiteRook.new
+            end
+
+            it { is_expected.not_to be_valid }
+          end
+        end
+      end
+
       context 'when the king has already moved' do
         before do
           board.data[0][0] = black_king
         end
-        it 'returns false' do
-          expect(black_castling).not_to be_valid
-        end
+
+        it { is_expected.not_to be_valid }
+      end
+    end
+
+    context 'when moving to the right' do
+    end
+
+    context 'when moving to a wrong square' do
+      context 'when moving towards left' do
+        let(:end_position) { [0, 1] }
+        it { is_expected.not_to be_valid }
+      end
+
+      context 'when moving towards right' do
+        let(:end_position) { [0, 5] }
+        it { is_expected.not_to be_valid }
       end
     end
   end
