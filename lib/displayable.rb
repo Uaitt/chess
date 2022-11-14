c738d717ff7e58156be0e229c0d58126e4bf2dd1
Lorenzo Zabot <lorenzozabot@gmail.com>
2022-11-14 10:05:04 +0100

    Reorgranize methods

--- lib/displayable.rb
+++ lib/displayable.rb
@@ -5,11 +5,15 @@ module Displayable
   def initial_instructions
     puts 'Welcome to CLI Chess!'
     puts 'This game can be played Human vs Human or Human vs Computer'
-    puts 'Be aware that first player color will be white, second player color will be black'
-    puts "Type \'yes\' if you want to play Human vs Human, anything else if you want to challenge a Computer"
+    puts 'Type \'new\' if you want to play a game from scratch, anything else if you want to load a saved game'
   end
 
-  def ask_name
+  def players_instruction
+    puts 'Be aware that first player color will be white, second player color will be black'
+    puts 'Type \'human\' if you want to play Human vs Human, anything else if you want to challenge a Computer'
+  end
+
+  def ask_for_name
     puts "Enter the name of the #{order} player"
   end
 end
