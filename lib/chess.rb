dcc29d05c4455ad98de185abadffbaba4bedd95b
Lorenzo Zabot <lorenzozabot@gmail.com>
2022-11-14 10:11:36 +0100

    Create initial scaffolding

---
+++ lib/chess.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+require_relative 'displayable'
+
+# this class represents the game of chess
+class Chess
+  include Displayable
+
+  def initialize
+    @board = Board.new
+    # @serializer = Serializer.new
+  end
+
+  def start
+    initial_instructions
+    gets.chomp == 'new' ? setup : restore_game # to do later
+    play
+  end
+
+  private
+
+  def setup
+    players_instruction
+    add_players
+    @board.set
+  end
+
+  def add_players
+    user_choice = gets.chomp
+    add_human(:first)
+    user_choice == 'human' ? add_human(:second) : add_computer
+  end
+
+  def add_human(order)
+    ask_for_name
+    if order == :first
+      @white_player = WhitePlayer.new(gets.chomp)
+    else
+      @black_player = BlackPlayer.new(gets.chomp)
+    end
+  end
+
+  def add_computer
+    @black_player = ComputerPlayer.new
+  end
+end
