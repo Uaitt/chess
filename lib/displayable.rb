4346c9948e714a6ae23e49a5e5aed0faf3989c0d
Lorenzo Zabot <lorenzozabot@gmail.com>
2022-11-14 09:26:13 +0100

    Fix typo

--- lib/displayable.rb
+++ lib/displayable.rb
@@ -5,6 +5,7 @@ module Displayable
   def initial_instructions
     puts 'Welcome to CLI Chess!'
     puts 'This game can be played Human vs Human or Human vs Computer'
-    puts 'Type \'yes\' if you want to play Human vs Human, anything else if you want to challenge a Computer'
+    puts 'Be aware that first player color will be white, second player color will be black'
+    puts "Type \'yes\' if you want to play Human vs Human, anything else if you want to challenge a Computer"
   end
 end
