ae4c0a872f0faf708b19d30c7b022aba8ed7d475
Lorenzo Zabot <lorenzozabot@gmail.com>
2022-11-15 09:05:21 +0100

    Add player files

---
+++ lib/players/white_human_player.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+# this class represents a human player that controls the white pieces
+class WhiteHumanPlayer
+  def initialize(name)
+    @name = name
+    @color = :white
+  end
+end
