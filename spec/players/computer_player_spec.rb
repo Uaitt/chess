22730568e8d5f6aa42965e8290e6bd25cf8f1544
Lorenzo Zabot <lorenzozabot@gmail.com>
2022-11-17 16:43:01 +0100

    Add test for API

---
+++ spec/players/computer_player_spec.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require 'require_all'
+
+require_all 'lib'
+
+describe ComputerPlayer do
+  subject(:computer_player) { described_class.new(board) }
+  let(:board) { Board.new }
+  describe '#play_round' do
+    context 'when the board was just set' do
+      before do
+        board.set
+        computer_player.play_round
+      end
+
+      it 'moves either a black pawn or a black knight' do
+        expect(board.data[0..1].flatten).to include(a_kind_of(NilPiece))
+      end
+    end
+  end
+end
