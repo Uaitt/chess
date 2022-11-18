6e9ec4d75668e6ef3dab1ded79a5516c1c394835
Lorenzo <97030518+Uaitt@users.noreply.github.com>
2022-11-18 16:23:08 +0100

    Fix typo in top level documentation

--- lib/players/computer_player.rb
+++ lib/players/computer_player.rb
@@ -2,7 +2,7 @@
 
 require_relative '../display/player_display'
 
-# this class represents am AI computer player that controls the black pieces in chess
+# this class represents an AI computer player that controls the black pieces in chess
 class ComputerPlayer
   include PlayerDisplay
 
