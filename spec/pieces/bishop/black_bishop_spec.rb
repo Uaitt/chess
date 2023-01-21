2aa25b2ff3e9f0588494101abce6bef08d710b3d
Lorenzo Zabot <lorenzozabot@gmail.com>
2023-01-21 15:41:22 +0100

    Load spec_helper on paths

--- spec/pieces/bishop/black_bishop_spec.rb
+++ spec/pieces/bishop/black_bishop_spec.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require_relative '../../spec_helper'
 require_relative '../../../lib/chess/pieces/bishop/black_bishop'
 
 describe BlackBishop do
--- spec/pieces/bishop/white_bishop_spec.rb
+++ spec/pieces/bishop/white_bishop_spec.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require_relative '../../spec_helper'
 require_relative '../../../lib/chess/pieces/bishop/white_bishop'
 
 describe WhiteBishop do
